567b9e93cc1ede5f7df29d664768cf2afd395747
Steve Alexander <steve@z3u.com>
2003-06-05 12:41:55 +0000

    ZopeContainerAdapter is now no longer used in unit tests.

--- tests/placelesssetup.py
+++ tests/placelesssetup.py
@@ -13,18 +13,15 @@
 ##############################################################################
 """Unit test logic for setting up and tearing down basic infrastructure
 
-$Id: placelesssetup.py,v 1.1 2003/06/04 14:57:57 stevea Exp $
+$Id: placelesssetup.py,v 1.2 2003/06/05 12:41:55 stevea Exp $
 """
 from zope.component.adapter import provideAdapter
 from zope.app.container.zopecontainer import ZopeContainerDecorator
-from zope.app.container.zopecontainer import ZopeContainerAdapter
 from zope.app.interfaces.context import IZopeContextWrapper
-from zope.app.interfaces.container import IContainer, IZopeContainer
+from zope.app.interfaces.container import IContainer
 
 class PlacelessSetup:
 
     def setUp(self):
         provideAdapter(
             IContainer, IZopeContextWrapper, ZopeContainerDecorator)
-        provideAdapter(
-            IContainer, IZopeContainer, ZopeContainerAdapter)
