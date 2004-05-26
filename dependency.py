2e8a7449d996ac8422b7812a1c9493302f515038
Jim Fulton <jim@zope.com>
2004-05-26 11:27:37 +0000

    Changed subscribers to be handlers, meaning that most event handlers are now just simple handler functions.  This is a work in progress (on this branch).

---
+++ dependency.py
@@ -0,0 +1,33 @@
+##############################################################################
+#
+# Copyright (c) 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Objects that take care of annotating dublin core meta data times
+
+$Id$
+"""
+
+from zope.app import zapi
+from zope.app.dependable.interfaces import IDependable, DependencyError
+from zope.proxy import removeAllProxies
+
+def CheckDependency(event):
+    object = removeAllProxies(event.object)
+    dependency = IDependable(object, None)
+    if dependency is not None:
+        dependents = dependency.dependents()
+        if dependents:
+            objectpath = zapi.getPath(event.object)
+            raise DependencyError("Removal of object (%s)"
+                                  " which has dependents (%s)"
+                                  % (objectpath,
+                                     ", ".join(dependents)))
