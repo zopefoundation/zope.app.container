e0a9fa1b13450abbb2a0fa42685e29c5b901d54c
Stephan Richter <stephan.richter@gmail.com>
2004-07-09 15:45:50 +0000

    Removed XXX.
    Converted XXX to statement.

--- browser/find.py
+++ browser/find.py
@@ -15,14 +15,13 @@
 
 $Id$
 """
-# XXX this needs to be looked up in a registry
 from zope.app.container.find import SimpleIdFindFilter
 from zope.app.container.interfaces import IFind
 from zope.app.traversing.api import getName
 from zope.component import getView
 from zope.app.publisher.browser import BrowserView
 
-# XXX very simple implementation right now
+# Very simple implementation right now
 class Find(BrowserView):
 
     def findByIds(self, ids):
