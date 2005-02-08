b1eb1be16bc196dba0c84148069ee1709f20883b
Dominik Huber <dominik.huber@ideell.ch>
2005-02-08 18:16:50 +0000

    The browser:containerViews directive does now provide an optional layer attribute.
    Partially fixed issue #306 (Problem 2: Browser page and view directive cannot register serveral pages views within different layers for the same for, menu and title attributes.)

--- browser/metaconfigure.py
+++ browser/metaconfigure.py
@@ -18,8 +18,10 @@ $Id$
 
 __docformat__ = 'restructuredtext'
 
+from zope.app.component.fields import LayerField
 from zope.interface import Interface
 from zope.configuration.fields import GlobalInterface
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 from zope.schema import Id
 from zope.app.publisher.browser.viewmeta import page, view
 from zope.app.container.browser.contents import Contents
@@ -51,8 +53,16 @@ class IContainerViews(Interface):
         title=u"The permission needed for add page.",
         required=False)
 
+    layer = LayerField(
+        title=_("The layer the view is in."),
+        description=_("""A skin is composed of layers. It is common to put 
+        skin specific views in a layer named after the skin. If the 'layer'
+        attribute is not supplied, it defaults to 'default'."""),
+        required=False
+        )
 
-def containerViews(_context, for_, contents=None, add=None, index=None):
+
+def containerViews(_context, for_, contents=None, add=None, index=None, layer=IDefaultBrowserLayer):
     """Set up container views for a given content type."""
 
     if for_ is None:
@@ -61,16 +71,16 @@ def containerViews(_context, for_, contents=None, add=None, index=None):
     if contents is not None:
         from zope.app.menus import zmi_views
         page(_context, name='contents.html', permission=contents,
-             for_=for_, class_=Contents, attribute='contents',
+             for_=for_, layer=layer, class_=Contents, attribute='contents',
              menu=zmi_views, title=_('Contents'))
 
     if index is not None:
         page(_context, name='index.html', permission=index, for_=for_,
-             class_=Contents, attribute='index')
+             layer=layer, class_=Contents, attribute='index')
 
     if add is not None:
         from zope.app.menus import zmi_actions
-        viewObj = view(_context, name='+', menu=zmi_actions,
+        viewObj = view(_context, name='+', layer=layer, menu=zmi_actions,
                        title=_('Add'), for_=for_, permission=add,
                        class_=Adding)
         viewObj.page(_context, name='index.html', attribute='index')
