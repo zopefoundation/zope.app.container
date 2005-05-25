c88a9fc2b147fd9a23f81f853997911d21d169a6
Egon Frerich <e.frerich@nord-com.net>
2005-05-25 20:01:44 +0000

    fixed typo

--- contained.py
+++ contained.py
@@ -11,7 +11,7 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-"""Classes to support implenting `IContained`
+"""Classes to support implementing `IContained`
 
 $Id$
 """
