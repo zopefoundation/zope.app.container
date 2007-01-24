6e0c64fbe9c35a10aa9475463a2847cd4f44da14
Marius Gedminas <marius@gedmin.as>
2007-01-24 19:37:00 +0000

    Make the ZCMLLayer tear down support optional, disabled by default -- it breaks 3rd party apps, which is not nice.

--- testing.py
+++ testing.py
@@ -3,5 +3,5 @@ from zope.app.testing.functional import ZCMLLayer
 
 AppContainerLayer = ZCMLLayer(
     os.path.join(os.path.split(__file__)[0], 'ftesting.zcml'),
-    __name__, 'AppContainerLayer')
+    __name__, 'AppContainerLayer', allow_teardown=True)
 
